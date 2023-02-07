0ce06fa93e65aa0ce378d56a17f022bed1c20391
geocrunchy <geocrunchy@gmail.com>
2023-02-07 18:33:01 +0300

    Adding hhs testss

--- README.md
+++ README.md
@@ -3,4 +3,21 @@
 
 <!-- badges: start -->
 [![R-CMD-check](https://github.com/mhkhan27/addindicators/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/mhkhan27/addindicators/actions/workflows/R-CMD-check.yaml)
+[![codecov](https://codecov.io/gh/mhkhan27/addindicators/branch/master/graph/badge.svg?token=RlTJbum32D)](https://codecov.io/gh/mhkhan27/addindicators)
 <!-- badges: end -->
+
+## Overview
+
+addindicators is designed for creating and checking (occasionally)
+composite indicators like Food Consumption Score (FCS) or Household
+Hunger Score (HHS)
+
+## Installation
+
+You can install the development version from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("impact-initiatives/addindicators")
+```
--- tests/testthat/test-add_composite.R
+++ tests/testthat/test-add_composite.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(dplyr)
 
-# testing -----------------------------------------------------------------
+# testing fcs-----------------------------------------------------------------
 
 testthat::test_that("Check_fcs",{
 
@@ -82,5 +82,33 @@ testthat::test_that("Check make_fcs",{
 
   testthat::expect_equal(food_consumption_df |> ncol()+2, ncol(res$result) )
 
+})
+
+
+# test HHS ----------------------------------------------------------------
+
+testthat::test_that("add_hhs",{
+
+  add_result <- evaluate_promise(
+    add_hhs(dataset = food_consumption_df,new_colname = "hhs",
+            hhs1a = "no_food_to_eat_because_of_lack_of_resources",
+            hhs1b = "how_often_no_food_to_eat_because_of_lack_of_resources",
+            hhs2a = "hh_memeber_sleep_hungry",
+            hhs2b = "how_often_hh_memeber_sleep_hungry",
+            hhs3a = "hh_member_go_a_whole_day_and_night_without_eating",
+            hhs3b = "how_often_hh_member_go_a_whole_day_and_night_without_eating",
+            frequency_choice = c("rarely_1_2_times", "sometimes_3_10_times", "often_10_plus_times"))
+  )
+
+  expect_true("hhs_cat" %in% names(add_result$result))
+  expect_true("hhs_score" %in% names(add_result$result))
+  expect_error(add_hhs(dataset = food_consumption_df,new_colname = "hhs",
+                       hhs1a = "no_food_to_eat_because_of_lack_of_resourc",
+                       hhs1b = "how_often_no_food_to_eat_because_of_lack_of_resources",
+                       hhs2a = "hh_memeber_sleep_hungry",
+                       hhs2b = "how_often_hh_memeber_sleep_hungry",
+                       hhs3a = "hh_member_go_a_whole_day_and_night_without_eating",
+                       hhs3b = "how_often_hh_member_go_a_whole_day_and_night_without_eating",
+                       frequency_choice = c("rarely_1_2_times", "sometimes_3_10_times", "often_10_plus_times")))
 })
 
