fd5a75adf9ed6e00f52b67fe55008a21300ad640
yannsay-impact <79831444+yannsay-impact@users.noreply.github.com>
2023-10-17 12:34:58 +0200

    adding review_variables

---
+++ R/review_variables.R
@@ -0,0 +1,155 @@
+#' Review 1 column comparing it to another one and spots differences
+#'
+#' @param dataset A dataset to be check.
+#' @param column_to_review Name of the column to review.
+#' @param column_to_compare_with Name of the column to compare with.
+#' @param prefix Prefix to be used for the review and comment column. Default is "review".
+#' @param return_dataset Logical, if the result table should be returned. Default is "FALSE".
+#'
+#' @return The review table, or the review table added to the results.
+#' @export
+#'
+#' @examples
+#' test_numeric <- data.frame(
+#'   test = c(
+#'     "test equality",
+#'     "test difference",
+#'     "test Missing in y",
+#'     "test Missing in x",
+#'     "test equality rounding in x",
+#'     "test equality rounding in y",
+#'     "test difference rounding in x",
+#'     "test difference rounding in y"
+#'   ),
+#'   var_x = c(0, 1, 2, NA, 0.00019, 0.0002, 0.00035, 0.0003),
+#'   var_y = c(0, 2, NA, 3, 0.0002, 0.00019, 0.0003, 0.00035)
+#' )
+#' review_one_variable(test_numeric,
+#'   column_to_review = "var_x",
+#'   column_to_compare_with = "var_y"
+#' )
+review_one_variable <- function(dataset,
+                                column_to_review,
+                                column_to_compare_with,
+                                prefix = "review",
+                                return_dataset = FALSE) {
+  if (!column_to_review %in% names(dataset)) {
+    msg <- glue::glue("Cannot find ", column_to_review, ".")
+    stop(msg)
+  }
+  if (!column_to_compare_with %in% names(dataset)) {
+    msg <- glue::glue("Cannot find ", column_to_compare_with, ".")
+    stop(msg)
+  }
+
+  review_name <- paste0(prefix, "_check_", column_to_review)
+  if (review_name %in% names(dataset)) {
+    msg <- glue::glue(review_name, " already exists.")
+    stop(msg)
+  }
+
+  comment_name <- paste0(prefix, "_comment_", column_to_review)
+  if (comment_name %in% names(dataset)) {
+    msg <- glue::glue(comment_name, " already exists.")
+    stop(msg)
+  }
+
+  if (typeof(dataset[[column_to_review]]) %in% c("double", "integer")) {
+    review_variable <- round(as.numeric(dataset[[column_to_review]]), 4) ==
+      round(as.numeric(dataset[[column_to_compare_with]]), 4)
+  } else if (typeof(dataset[[column_to_review]]) %in% c("character")) {
+    review_variable <- dataset[[column_to_review]] ==
+      dataset[[column_to_compare_with]]
+  }
+
+  review_variable[is.na(review_variable)] <- is.na(dataset[[column_to_review]][is.na(review_variable)]) &
+    is.na(dataset[[column_to_compare_with]][is.na(review_variable)])
+
+  comments_variable <- dplyr::case_when(
+    review_variable ~ "Same results",
+    is.na(dataset[[column_to_review]]) ~ glue::glue("Missing in ", column_to_review),
+    is.na(dataset[[column_to_compare_with]]) ~ glue::glue("Missing in ", column_to_compare_with),
+    TRUE ~ "Different results"
+  )
+
+  review_table <- data.frame(review_variable, comments_variable)
+
+  names(review_table) <- c(review_name, comment_name)
+
+  if (return_dataset) {
+    dataset <- cbind(dataset, review_table)
+    return(dataset)
+  } else {
+    return(review_table)
+  }
+}
+
+#' Review columns comparing it to another set of columns and spots differences
+#'
+#' review_variables is a wrapper around review_one_variable
+#'
+#' @param dataset A dataset to be check.
+#' @param columns_to_review Vectors of columns to review (should be paired with
+#' columns_to_compare_with).
+#' @param columns_to_compare_with Vectors of columns to compare with  (should be paired with
+#' columns_to_review).
+#' @param prefix Prefix to be used for the review and comment column. Default is "review"
+#'
+#' @return A list with two objects:
+#'   - the result table the review and comment columns
+#'   - the review table
+#' @export
+#'
+#' @examples
+#' test_numeric_2_var <- data.frame(
+#'   test = c(
+#'     "test equality",
+#'     "test difference",
+#'     "test Missing in y",
+#'     "test Missing in x",
+#'     "test equality rounding in x",
+#'     "test equality rounding in y",
+#'     "test difference rounding in x",
+#'     "test difference rounding in y"
+#'   ),
+#'   stat_col_one.x = c(0, 1, 2, NA, 0.00019, 0.0002, 0.00035, 0.0003),
+#'   stat_col_two.x = c(0, 1, 2, NA, 0.00019, 0.0002, 0.00035, 0.0003),
+#'   stat_col_one.y = c(0, 2, NA, 3, 0.0002, 0.00019, 0.0003, 0.00035),
+#'   stat_col_two.y = c(0, 2, NA, 3, 0.0002, 0.00019, 0.0003, 0.00035)
+#' )
+#'
+#' actual_results <- review_variables(test_numeric_2_var,
+#'   columns_to_review = c("stat_col_one.x", "stat_col_two.x"),
+#'   columns_to_compare_with = c("stat_col_one.y", "stat_col_two.y")
+#' )
+review_variables <- function(dataset,
+                             columns_to_review,
+                             columns_to_compare_with,
+                             prefix = "review") {
+  list_of_reviews <- purrr::map2(
+    columns_to_review,
+    columns_to_compare_with,
+    ~ review_one_variable(dataset,
+      column_to_review = .x,
+      column_to_compare_with = .y,
+      prefix = prefix
+    )
+  )
+  review_table <- list_of_reviews %>%
+    purrr::map2(.y = columns_to_review, ~ dplyr::mutate(.x, variable = .y)) %>%
+    purrr::map(.f = ~ `names<-`(.x, c(
+      paste0(prefix, "_check"),
+      paste0(prefix, "_comment"),
+      "variable"
+    ))) %>%
+    do.call(rbind, .) %>%
+    dplyr::select(variable, review_check, review_comment)
+  dataset <- dataset %>%
+    cbind(list_of_reviews)
+
+  list_to_return <- list(
+    dataset = dataset,
+    review_table = review_table
+  )
+  return(list_to_return)
+}
---
+++ tests/testthat/test-review_variables.R
@@ -0,0 +1,266 @@
+test_that("review_one_variable returns correct results for numeric values", {
+  test_numeric <- data.frame(
+    test = c(
+      "test equality",
+      "test difference",
+      "test Missing in y",
+      "test Missing in x",
+      "test equality missing in both",
+      "test equality rounding in x",
+      "test equality rounding in y",
+      "test difference rounding in x",
+      "test difference rounding in y"
+    ),
+    var_x = c(0, 1, 2, NA, NA, 0.00019, 0.0002, 0.00035, 0.0003),
+    var_y = c(0, 2, NA, 3, NA, 0.0002, 0.00019, 0.0003, 0.00035)
+  )
+  expected_results <- data.frame(
+    review_check_var_x = c(TRUE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, FALSE, FALSE),
+    review_comment_var_x = c(
+      "Same results",
+      "Different results",
+      "Missing in var_y",
+      "Missing in var_x",
+      "Same results",
+      "Same results",
+      "Same results",
+      "Different results",
+      "Different results"
+    )
+  )
+  actual_results <- review_one_variable(test_numeric,
+    column_to_review = "var_x",
+    column_to_compare_with = "var_y"
+  )
+  expect_equal(actual_results, expected_results)
+
+  actual_results_returns_table <- review_one_variable(test_numeric,
+    column_to_review = "var_x",
+    column_to_compare_with = "var_y",
+    return_dataset = TRUE
+  )
+  expect_equal(actual_results_returns_table, cbind(test_numeric, expected_results))
+
+  new_prefix_expected_results <- expected_results %>%
+    `names<-`(c("my_review_check_var_x", "my_review_comment_var_x"))
+  new_prefix_actual_results <- review_one_variable(test_numeric,
+    column_to_review = "var_x",
+    column_to_compare_with = "var_y",
+    prefix = "my_review"
+  )
+  expect_equal(new_prefix_actual_results, new_prefix_expected_results)
+})
+
+test_that("review_one_variable returns correct results for categorical values", {
+  test_categorical <- data.frame(
+    test = c(
+      "test equality",
+      "test difference",
+      "test Missing in y",
+      "test Missing in x",
+      "test equality missing in both"
+    ),
+    var_x = c("A", "B", "C", NA, NA),
+    var_y = c("A", "A", NA, "D", NA)
+  )
+  expected_results <- data.frame(
+    review_check_var_x = c(TRUE, FALSE, FALSE, FALSE, TRUE),
+    review_comment_var_x = c(
+      "Same results",
+      "Different results",
+      "Missing in var_y",
+      "Missing in var_x",
+      "Same results"
+    )
+  )
+  actual_results <- review_one_variable(test_categorical,
+    column_to_review = "var_x",
+    column_to_compare_with = "var_y"
+  )
+  expect_equal(actual_results, expected_results)
+})
+
+test_that("review_one_variable returns correct error messages", {
+  test_numeric <- data.frame(
+    test = c(
+      "test equality",
+      "test difference",
+      "test Missing in y",
+      "test Missing in x",
+      "test equality rounding in x",
+      "test equality rounding in y",
+      "test difference rounding in x",
+      "test difference rounding in y"
+    ),
+    var_x = c(0, 1, 2, NA, 0.00019, 0.0002, 0.00035, 0.0003),
+    var_y = c(0, 2, NA, 3, 0.0002, 0.00019, 0.0003, 0.00035),
+    review_check_var_x = c(TRUE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE),
+    review_comment_var_x = c(
+      "Same results",
+      "Different results",
+      "Missing in var_y",
+      "Missing in var_x",
+      "Same results",
+      "Same results",
+      "Different results",
+      "Different results"
+    )
+  )
+
+
+  # cannot find x
+  expect_error(
+    review_one_variable(
+      test_numeric %>%
+        dplyr::select(!all_of(c("review_check_var_x", "review_comment_var_x"))),
+      column_to_review = "VAR_X", column_to_compare_with = "var_y"
+    ),
+    "Cannot find VAR_X."
+  )
+  # cannot find y
+  expect_error(
+    review_one_variable(
+      test_numeric %>%
+        dplyr::select(!all_of(c("review_check_var_x", "review_comment_var_x"))),
+      column_to_review = "var_x", column_to_compare_with = "VAR_Y"
+    ),
+    "Cannot find VAR_Y."
+  )
+  # review_xxx already exist
+  expect_error(
+    review_one_variable(
+      test_numeric %>%
+        dplyr::select(!all_of(c("review_comment_var_x"))),
+      column_to_review = "var_x", column_to_compare_with = "var_y"
+    ),
+    "review_check_var_x already exists."
+  )
+
+  expect_error(
+    review_one_variable(
+      test_numeric %>%
+        dplyr::select(!all_of(c("review_check_var_x"))),
+      column_to_review = "var_x", column_to_compare_with = "var_y"
+    ),
+    "review_comment_var_x already exists."
+  )
+})
+
+test_that("review_variables returns correct numerical results", {
+  test_numeric_2_var <- data.frame(
+    test = c(
+      "test equality",
+      "test difference",
+      "test Missing in y",
+      "test Missing in x",
+      "test equality rounding in x",
+      "test equality rounding in y",
+      "test difference rounding in x",
+      "test difference rounding in y"
+    ),
+    stat_col_one.x = c(0, 1, 2, NA, 0.00019, 0.0002, 0.00035, 0.0003),
+    stat_col_two.x = c(0, 1, 2, NA, 0.00019, 0.0002, 0.00035, 0.0003),
+    stat_col_one.y = c(0, 2, NA, 3, 0.0002, 0.00019, 0.0003, 0.00035),
+    stat_col_two.y = c(0, 2, NA, 3, 0.0002, 0.00019, 0.0003, 0.00035)
+  )
+
+  review_check_columm <- c(TRUE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE)
+  review_comment_column_stat_col_one.x <- c(
+    "Same results",
+    "Different results",
+    "Missing in stat_col_one.y",
+    "Missing in stat_col_one.x",
+    "Same results",
+    "Same results",
+    "Different results",
+    "Different results"
+  )
+
+  review_comment_column_stat_col_two.x <- c(
+    "Same results",
+    "Different results",
+    "Missing in stat_col_two.y",
+    "Missing in stat_col_two.x",
+    "Same results",
+    "Same results",
+    "Different results",
+    "Different results"
+  )
+
+  expected_results_table <- test_numeric_2_var %>%
+    cbind(
+      review_check_stat_col_one.x = review_check_columm,
+      review_comment_stat_col_one.x = review_comment_column_stat_col_one.x,
+      review_check_stat_col_two.x = review_check_columm,
+      review_comment_stat_col_two.x = review_comment_column_stat_col_two.x
+    )
+
+  expected_review_table <- data.frame(
+    variable = c(rep("stat_col_one.x", 8), rep("stat_col_two.x", 8)),
+    review_check = rep(review_check_columm, 2),
+    review_comment = c(
+      review_comment_column_stat_col_one.x,
+      review_comment_column_stat_col_two.x
+    )
+  )
+
+  expected_results <- list(
+    dataset = expected_results_table,
+    review_table = expected_review_table
+  )
+
+  actual_results <- review_variables(test_numeric_2_var,
+    columns_to_review = c("stat_col_one.x", "stat_col_two.x"),
+    columns_to_compare_with = c("stat_col_one.y", "stat_col_two.y")
+  )
+
+  expect_equal(actual_results, expected_results)
+})
+
+test_that("review_variables returns correct results for categorical and numerical values", {
+  test_categorical_numerical <- data.frame(
+    test = c(
+      "test equality",
+      "test difference",
+      "test Missing in y",
+      "test Missing in x"
+    ),
+    var_x = c("A", "B", "C", NA),
+    var_y = c("A", "A", NA, "D"),
+    stat_col_one.x = c(0, 1, 2, NA),
+    stat_col_one.y = c(0, 1, 2, NA)
+  )
+  expected_results_table <- cbind(test_categorical_numerical,
+    review_check_var_x = c(TRUE, FALSE, FALSE, FALSE),
+    review_comment_var_x = c(
+      "Same results",
+      "Different results",
+      "Missing in var_y",
+      "Missing in var_x"
+    ),
+    review_check_stat_col_one.x = rep(TRUE, 4),
+    review_comment_stat_col_one.x = rep("Same results", 4)
+  )
+  expected_review_table <- data.frame(
+    variable = c(rep("var_x", 4), rep("stat_col_one.x", 4)),
+    review_check = c(TRUE, FALSE, FALSE, FALSE, rep(TRUE, 4)),
+    review_comment = c(
+      "Same results",
+      "Different results",
+      "Missing in var_y",
+      "Missing in var_x",
+      rep("Same results", 4)
+    )
+  )
+  expected_results <- list(
+    dataset = expected_results_table,
+    review_table = expected_review_table
+  )
+
+
+  actual_results <- review_variables(test_categorical_numerical,
+    columns_to_review = c("var_x", "stat_col_one.x"),
+    columns_to_compare_with = c("var_y", "stat_col_one.y")
+  )
+  expect_equal(actual_results, expected_results)
+})
